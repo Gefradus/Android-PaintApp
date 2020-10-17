fffef0c86477e892090d0a08cb421806bab806bb
dratwak <krzysztof.dratwa@ttsw.com.pl>
2020-10-17 19:16:15 +0200

    a

--- app/src/main/java/com/example/rysowanie/SaveImageFromCanvasViewToGallery.java
+++ app/src/main/java/com/example/rysowanie/SaveImageFromCanvasViewToGallery.java
@@ -1,8 +1,29 @@
 package com.example.rysowanie;
 
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.view.View;
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class SaveImageFromCanvasViewToGallery {
     public SaveImageFromCanvasViewToGallery(CanvasView canvasView)
     {
+        canvasView.setDrawingCacheEnabled(true);
+        canvasView.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+        Bitmap bitmap = canvasView.getDrawingCache();
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+        File file = new File(path+"/image.png");
+        FileOutputStream ostream;
+        try {
+            file.createNewFile();
+            ostream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, ostream);
+            ostream.flush();
+            ostream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
