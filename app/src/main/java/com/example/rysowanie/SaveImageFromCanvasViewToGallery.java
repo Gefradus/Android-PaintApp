c8ba62ff768f2c9b582236c47890e5660bcf1686
dratwak <krzysztof.dratwa@ttsw.com.pl>
2020-10-18 13:20:15 +0200

    new files names handling

--- app/src/main/java/com/example/rysowanie/SaveImageFromCanvasViewToGallery.java
+++ app/src/main/java/com/example/rysowanie/SaveImageFromCanvasViewToGallery.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.os.Environment;
 import android.view.View;
+import android.widget.Toast;
 import java.io.File;
 import java.io.FileOutputStream;
 
@@ -12,22 +13,31 @@ public class SaveImageFromCanvasViewToGallery
 {
     public SaveImageFromCanvasViewToGallery(Activity activity, CanvasView canvasView)
     {
-        canvasView.setDrawingCacheEnabled(true);
-        canvasView.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+        File file = createFile(activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES).getAbsolutePath());
 
-        String path = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES).getAbsolutePath();
-        File file = new File(path+"/image.png");
-        FileOutputStream ostream;
         try {
             file.createNewFile();
-            ostream = new FileOutputStream(file);
-            getBitmapFromView(canvasView).compress(Bitmap.CompressFormat.PNG, 100, ostream);
-            ostream.flush();
-            ostream.close();
-        } catch (Exception e) {
+            FileOutputStream outputStream = new FileOutputStream(file);
+            getBitmapFromView(canvasView).compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+            outputStream.flush();
+            outputStream.close();
+            Toast.makeText(activity, "Twój obraz został zapisany pomyślnie", Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+
+    private File createFile(String path)
+    {
+        int i = 0;
+        File file = null;
+        while(i == 0 || file.exists()) {
+            i++;
+            file = new File(path + "/image" + i + ".png");
+        }
+        return file;
     }
 
     private Bitmap getBitmapFromView(View view)
