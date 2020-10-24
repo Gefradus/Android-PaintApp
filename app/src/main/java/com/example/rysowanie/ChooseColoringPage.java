ce1e4bf47e21bc60aa1421e6c2dfd06c003e8e29
dratwak <krzysztof.dratwa@ttsw.com.pl>
2020-10-24 21:41:53 +0200

    redundant code deleted

--- app/src/main/java/com/example/rysowanie/ChooseColoringPage.java
+++ app/src/main/java/com/example/rysowanie/ChooseColoringPage.java
@@ -7,9 +7,9 @@ public class ChooseColoringPage
     public ChooseColoringPage(MainActivity activity)
     {
         int paintingPageEnum = activity.getIntent().getExtras().getInt("coloringPage");
-        int id = 0;
-
+        
         if(paintingPageEnum != 0) {
+            int id = 0;
             if (paintingPageEnum == R.id.image1) {
                 id = R.drawable.kolorowanka1;
             } else if (paintingPageEnum == R.id.image2) {
